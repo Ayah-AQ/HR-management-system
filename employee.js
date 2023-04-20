2d64f85af84b68367086097462f5ee73be781d2e
Aya Yazouri <84291148+Ayah-AQ@users.noreply.github.com>
2023-04-20 13:13:45 +0000

    part 1 is done

--- employee.js
+++ employee.js
@@ -1,11 +1,17 @@
 'use strict';
-function Employee(id, fullName, department, level, imageUrl, salary) {
+
+const add =document.getElementById("employee-form")
+add.addEventListener("submit", addEmployee)
+
+let employees = [];
+function Employee(id, fullName, department, level, imageUrl) {
   this.id = id;
   this.imageUrl = imageUrl;
   this.fullName = fullName;
   this.department = department;
   this.level = level;
-  // this.salary = salary;
+  this.salary = 0;
+  employees.push(this)
 }
 
 Employee.prototype.calculate= function () {
@@ -34,8 +40,222 @@ Employee.prototype.calculate= function () {
     
   }
   let netSal= Math.floor(randomSalary - (randomSalary * tax ))
-  return netSal
+  return this.salary=netSal
 }
+
+Employee.prototype.render = function () {
+  let employee=document.getElementById('container')
+  const imgEl = document.createElement('img');
+    const imgdiv=document.createElement('div');
+    imgdiv.setAttribute('class','img-content');
+    employee.appendChild(imgdiv);
+    imgdiv.appendChild(imgEl);
+    imgEl.src =this.imageUrl;
+    
+    const divEle = document.createElement('div');
+    employee.appendChild(divEle);
+    
+    
+    const depEle = document.createElement('p')
+    depEle.textContent = `Name: ${this.fullName} - ID: ${this.id}`;
+    divEle.appendChild(depEle);
+    
+    const levelEle = document.createElement('p')
+    levelEle.textContent = `Department: ${this.department} - Level: ${this.level}`;
+    divEle.appendChild(levelEle);
+    
+    const salEle = document.createElement('p');
+    salEle.textContent = this.calculate();
+    divEle.appendChild(salEle)
+    const ulEl = document.createElement('ul');
+    employee.appendChild(ulEl);
+  
+  }
+  
+let Ghazi  =   new Employee(1000,"ghazi samer","Administration","Senior", "https://github.com/LTUC/amman-prep-d13/blob/main/Class-08/lab/assets/Ghazi.jpg?raw=true" );
+let Lana   =   new Employee(1001,"Lana Ali","Finance","Senior","https://github.com/LTUC/amman-prep-d13/blob/main/Class-08/lab/assets/Lana.jpg?raw=true");
+let Tamara =   new Employee(1002,"Tamara Ayoub","Marketing","Senior","https://github.com/LTUC/amman-prep-d13/blob/main/Class-08/lab/assets/Tamara.jpg?raw=true");
+let Safi   =   new Employee(1003,"Safi Walid","Administration","Mid-Senior","https://github.com/LTUC/amman-prep-d13/blob/main/Class-08/lab/assets/Safi.jpg?raw=true");
+let Omar   =   new Employee(1004,"Omar Zaid	","Development","Senior","https://github.com/LTUC/amman-prep-d13/blob/main/Class-08/lab/assets/Omar.jpg?raw=true");
+let Rana   =   new Employee(1005,"Rana Saleh	","Development","Junior","https://github.com/LTUC/amman-prep-d13/blob/main/Class-08/lab/assets/Rana.jpg?raw=true");
+let Hadi   =   new Employee(1006,"Hadi Ahmad","Finance","Mid-Senior","https://github.com/LTUC/amman-prep-d13/blob/main/Class-08/lab/assets/Hadi.jpg?raw=true");
+
+  function companyEmployees (){
+    for(let i=0 ; i<employees.length ; i++){
+    
+        employees[i].calculate();
+        employees[i].render();
+    }
+    }
+    
+function randId(){
+  let randomId=Math.floor(1000 + Math.random() * 9000);
+    return randomId;
+}
+
+Employee.prototype.render = function () {
+  
+  if (this.department === 'Administration'){
+    // let employee = document.getElementsByClassName('adminstration');
+    let employee = document.getElementById('Administration');
+
+    console.log(employee)
+    const imgEl = document.createElement('img');
+    const imgdiv=document.createElement('div');
+    imgdiv.setAttribute('class','img-content');
+    console.log(imgEl)
+
+    employee.appendChild(imgdiv);
+    imgdiv.appendChild(imgEl);
+    imgEl.src =this.imageUrl;
+    
+    const divEle = document.createElement('div');
+    employee.appendChild(divEle);
+    
+    
+    const depEle = document.createElement('p')
+    depEle.textContent = `Name: ${this.fullName} - ID: ${this.id}`;
+    divEle.appendChild(depEle);
+    
+    const levelEle = document.createElement('p')
+    levelEle.textContent = `Department: ${this.department} - Level: ${this.level}`;
+    divEle.appendChild(levelEle);
+    
+    const salEle = document.createElement('p');
+    salEle.textContent = this.calculate();
+    divEle.appendChild(salEle)
+    const ulEl = document.createElement('ul');
+    employee.appendChild(ulEl);
+}
+
+if (this.department === 'Marketing'){
+  let employee = document.getElementById('Marketing');
+  
+    const imgEl = document.createElement('img');
+    const imgdiv=document.createElement('div');
+    imgdiv.setAttribute('class','img-content');
+    employee.appendChild(imgdiv);
+    imgdiv.appendChild(imgEl);
+    imgEl.src =this.imageUrl;
+    
+    const divEle = document.createElement('div');
+    employee.appendChild(divEle);
+    const depEle = document.createElement('p')
+    depEle.textContent = `Name: ${this.fullName} - ID: ${this.id}`;
+    divEle.appendChild(depEle);
+    
+    
+    const levelEle = document.createElement('p')
+    levelEle.textContent = `Department: ${this.department} - Level: ${this.level}`;
+    divEle.appendChild(levelEle);
+    
+    const salEle = document.createElement('p');
+    salEle.textContent = this.calculate();
+    divEle.appendChild(salEle)
+    
+    const ulEl = document.createElement('ul');
+    employee.appendChild(ulEl);
+  }
+  if (this.department === 'Development'){
+    let employee = document.getElementById('Development');
+    
+    const imgEl = document.createElement('img');
+    const imgdiv=document.createElement('div');
+    imgdiv.setAttribute('class','img-content');
+    employee.appendChild(imgdiv);
+          imgdiv.appendChild(imgEl);
+          imgEl.src =this.imageUrl;
+          
+          const divEle = document.createElement('div');
+          employee.appendChild(divEle);
+          
+          const depEle = document.createElement('p')
+          depEle.textContent = `Name: ${this.fullName} - ID: ${this.id}`;
+          divEle.appendChild(depEle);
+          
+          const levelEle = document.createElement('p')
+          levelEle.textContent = `Department: ${this.department} - Level: ${this.level}`;
+          divEle.appendChild(levelEle);
+          
+          
+          const salEle = document.createElement('p');
+          salEle.textContent = this.calculate();
+          divEle.appendChild(salEle)
+          
+          const ulEl = document.createElement('ul');
+          employee.appendChild(ulEl);
+        }
+        if (this.department === 'Finance'){
+          let employee = document.getElementById('Finance');
+          
+          const imgEl = document.createElement('img');
+          const imgdiv=document.createElement('div');
+          imgdiv.setAttribute('class','img-content');
+          employee.appendChild(imgdiv);
+          imgdiv.appendChild(imgEl);
+          imgEl.src =this.imageUrl;
+          
+          const divEle = document.createElement('div');
+          employee.appendChild(divEle);
+          
+          
+          const depEle = document.createElement('p')
+          depEle.textContent = `Name: ${this.fullName} - ID: ${this.id}`;
+          divEle.appendChild(depEle);
+          
+          const levelEle = document.createElement('p')
+          levelEle.textContent = `Department: ${this.department} - Level: ${this.level}`;
+          divEle.appendChild(levelEle);
+          
+          const salEle = document.createElement('p');
+          salEle.textContent = this.calculate();
+          divEle.appendChild(salEle)
+          
+          const ulEl = document.createElement('ul');
+          employee.appendChild(ulEl);
+        }
+      }
+      function saveData(employeee){
+        let saveArr = JSON.stringify(employeee);
+        localStorage.setItem('employees',saveArr);
+        em.innerHTML = `<p>${parsedData.someProperty}</p>`
+    }
+    
+    function getData (){
+        let getArr = localStorage.getItem('employees');
+        let objArr = JSON.parse(getArr);
+        // element.innerHTML = `<p>${objArr.this}</p>`
+        if (objArr != null){
+            for (let i = 7; i < objArr.length; i++) {
+                new Employee(objArr[i].id,objArr[i].fullName,objArr[i].department,objArr[i].level,objArr[i].imagrURL);
+            }
+        }
+        
+    }
+    
+      function addEmployee(e){
+        e.preventDefault()
+        let imageUrl   = e.target.imageUrl.value;
+        let fullName   = e.target.fullName.value;
+        let department = e.target.department.value;
+        let level      = e.target.level.value;
+        let id=randId();
+        const newEmployee = new Employee(id,fullName,  department, level, imageUrl)
+        console.log(newEmployee)
+        newEmployee.render()
+        newEmployee.calculate()
+        saveData(employees)
+
+  }
+    
+  companyEmployees()
+  getData();
+  saveData(employees);
+
+
+
+
+
 // Employee.prototype.render = function(){
 //  document.getElementById('container');
 //   // if(this.department=== "Administration"){
@@ -79,13 +299,6 @@ Employee.prototype.calculate= function () {
 // document.write("<br><br>")
 // }
 // }
-// let Ghazi  =   new Employee(1000,"ghazi samer","Administration","Senior", "https://github.com/LTUC/amman-prep-d13/blob/main/Class-08/lab/assets/Ghazi.jpg?raw=true" );
-// let Lana   =   new Employee(1001,"Lana Ali","Finance","Senior","https://github.com/LTUC/amman-prep-d13/blob/main/Class-08/lab/assets/Lana.jpg?raw=true");
-// let Tamara =   new Employee(1002,"Tamara Ayoub","Marketing","Senior","https://github.com/LTUC/amman-prep-d13/blob/main/Class-08/lab/assets/Tamara.jpg?raw=true");
-// let Safi   =   new Employee(1003,"Safi Walid","Administration","Mid-Senior","https://github.com/LTUC/amman-prep-d13/blob/main/Class-08/lab/assets/Safi.jpg?raw=true");
-// let Omar   =   new Employee(1004,"Omar Zaid	","Development","Senior","https://github.com/LTUC/amman-prep-d13/blob/main/Class-08/lab/assets/Omar.jpg?raw=true");
-// let Rana   =   new Employee(1005,"Rana Saleh	","Development","Junior","https://github.com/LTUC/amman-prep-d13/blob/main/Class-08/lab/assets/Rana.jpg?raw=true");
-// let Hadi   =   new Employee(1006,"Hadi Ahmad","Finance","Mid-Senior","https://github.com/LTUC/amman-prep-d13/blob/main/Class-08/lab/assets/Hadi.jpg?raw=true");
 
 // Ghazi.render();
 // Lana.render()
@@ -94,145 +307,3 @@ Employee.prototype.calculate= function () {
 // Omar.render()
 // Rana.render()
 // Hadi.render()
-
-function randId(){
-  let randomId=Math.floor(1000 + Math.random() * 9000);
-    return randomId;
-}
-
-Employee.prototype.render = function () {
-  
-  // let employee= (this.department === 'Administration'? document.getElementById('Administration') : this.department === 'Marketing'? document.getElementById('Marketing') )
-  if (this.department === 'Administration'){
-  let employee = document.getElementById('Administration');
-
- const imgEl = document.createElement('img');
-  const imgdiv=document.createElement('div');
-  imgdiv.setAttribute('class','img-content');
-  employee.appendChild(imgdiv);
-  imgdiv.appendChild(imgEl);
-  imgEl.src =this.imageUrl;
-  
-  const divEle = document.createElement('div');
-  employee.appendChild(divEle);
-
-  
-  const depEle = document.createElement('p')
-  depEle.textContent = `Name: ${this.fullName} - ID: ${this.id}`;
-  divEle.appendChild(depEle);
-  
-  const levelEle = document.createElement('p')
-  levelEle.textContent = `Department: ${this.department} - Level: ${this.level}`;
-  divEle.appendChild(levelEle);
-  
-  const salEle = document.createElement('p');
-  salEle.textContent = this.calculate();
-  divEle.appendChild(salEle)
-  const ulEl = document.createElement('ul');
-  employee.appendChild(ulEl);
-  }
-  
-  if (this.department === 'Marketing'){
-    // let  employee = document.getElementById('container');
-    let employee = document.getElementById('Marketing');
-    
-   const imgEl = document.createElement('img');
-    const imgdiv=document.createElement('div');
-    imgdiv.setAttribute('class','img-content');
-    employee.appendChild(imgdiv);
-    imgdiv.appendChild(imgEl);
-    imgEl.src =this.imageUrl;
-  
-    const divEle = document.createElement('div');
-    employee.appendChild(divEle);
-    const depEle = document.createElement('p')
-    depEle.textContent = `Name: ${this.fullName} - ID: ${this.id}`;
-    divEle.appendChild(depEle);
-    
-    
-    const salEle = document.createElement('p');
-    salEle.textContent = this.calculate();
-    divEle.appendChild(salEle)
-
-      const levelEle = document.createElement('p')
-    levelEle.textContent = `Department: ${this.department} - Level: ${this.level}`;
-    divEle.appendChild(levelEle);
-  
-    const ulEl = document.createElement('ul');
-    employee.appendChild(ulEl);
-    }
-    if (this.department === 'Development'){
-      // let  employee = document.getElementById('container');
-      let employee = document.getElementById('Development');
-    
-     const imgEl = document.createElement('img');
-     const imgdiv=document.createElement('div');
-      imgdiv.setAttribute('class','img-content');
-      employee.appendChild(imgdiv);
-      imgdiv.appendChild(imgEl);
-      imgEl.src =this.imageUrl;
-    
-      const divEle = document.createElement('div');
-      employee.appendChild(divEle);
-      
-      const depEle = document.createElement('p')
-      depEle.textContent = `Name: ${this.fullName} - ID: ${this.id}`;
-      divEle.appendChild(depEle);
-      
-      const levelEle = document.createElement('p')
-      levelEle.textContent = `Department: ${this.department} - Level: ${this.level}`;
-      divEle.appendChild(levelEle);
-      
-      
-      const salEle = document.createElement('p');
-  salEle.textContent = this.calculate();
-  divEle.appendChild(salEle)
-
-      const ulEl = document.createElement('ul');
-      employee.appendChild(ulEl);
-      }
-  if (this.department === 'Finance'){
-        // let  employee = document.getElementById('container');
-        let employee = document.getElementById('Finance');
-       
-        const imgEl = document.createElement('img');
-        const imgdiv=document.createElement('div');
-        imgdiv.setAttribute('class','img-content');
-        employee.appendChild(imgdiv);
-        imgdiv.appendChild(imgEl);
-        imgEl.src =this.imageUrl;
-     
-        const divEle = document.createElement('div');
-        employee.appendChild(divEle);
-      
-        
-        const depEle = document.createElement('p')
-        depEle.textContent = `Name: ${this.fullName} - ID: ${this.id}`;
-        divEle.appendChild(depEle);
-        
-        const levelEle = document.createElement('p')
-        levelEle.textContent = `Department: ${this.department} - Level: ${this.level}`;
-        divEle.appendChild(levelEle);
-        
- const salEle = document.createElement('p');
-  salEle.textContent = this.calculate();
-  divEle.appendChild(salEle)
-
-        const ulEl = document.createElement('ul');
-        employee.appendChild(ulEl);
-        }
-}
-function addEmployee(e){
-e.preventDefault()
-let imageUrl   = e.target.imageUrl.value;
-let fullName   = e.target.fullName.value;
-let department = e.target.department.value;
-let level      = e.target.level.value;
-let id=randId();
-const newEmployee = new Employee(id,fullName,  department, level, imageUrl)
-console.log(newEmployee)
-newEmployee.render()
-
-}
-const add =document.getElementById("employee-form")
-add.addEventListener("submit", addEmployee)
